0437af2daacb63b05ddf40e243519948ced31241
yuliaraitsyna <yuliaraitsyna@gmail.com>
2023-12-06 13:41:29 +0300

    drawing

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ clearButton.innerHTML = "clear";
 const slider = document.getElementById("slider");
 slider.value = 2;
 const field = document.getElementById("field");
+let currentColour;
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 function ChangeSliderValue()
 {
@@ -28,8 +32,15 @@ function SetUpGrid(size)
     {
         const cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.addEventListener('mouseover', ChangeColour);
+        cell.addEventListener('mousedown', ChangeColour);
         field.appendChild(cell);
     }
+}
+function ChangeColour(e)
+{
+    if (e.type === 'mouseover' && !mouseDown) return;
+    e.currentTarget.style.backgroundColor = currentColour;
 }
 function removeCells() {
     while(field.firstChild) {
@@ -52,41 +63,69 @@ const colours = [];
 for (let i = 1; i <= 6; i++) {
     let colour = document.getElementById("colour" + i);
     let colourEnum;
+    let hex;
     switch (i) {
         case 1:
             colourEnum = 'Purple';
+            hex = "#9745FF";
             break;
         case 2:
             colourEnum = 'Blue';
+            hex = "#82A5FF";
             break;
         case 3:
             colourEnum = 'Pink';
+            hex = "#FE7DDA";
             break;
         case 4:
             colourEnum = 'Green';
+            hex = "#94CC86";
             break;
         case 5:
             colourEnum = 'Yellow';
+            hex = "#EFE37D";
             break;
         case 6:
             colourEnum = 'Black';
+            hex = "#000000";
             break;
         default:
             colourEnum = 'Black';
+            hex = "#000000";
     }
-    colours.push({ HTMLObject: colour, ColourValue: colourEnum });
+    colours.push({ HTMLObject: colour, ColourValue: colourEnum, HEX:hex});
 }
 
-function ColourChoice(event) {
-    const colourDiv = event.currentTarget;
+function ColourChoice(e) {
+    const colourDiv = e.currentTarget;
+    eraseButton.style.border="none";
     colourDiv.style.border = "solid white 2px";
     colours.forEach(colour =>{
         if(colour.HTMLObject !== colourDiv)
         {
             colour.HTMLObject.style.border = "none";
         }
+        else
+        {
+            currentColour = colour.HEX;
+        }
     });
 }
+eraseButton.addEventListener('click', () =>
+{
+    eraseButton.style.border = "solid white 2px";
+    currentColour="#FFFFFF";
+    colours.forEach(colour =>
+        {
+            colour.HTMLObject.style.border ="none";
+        })
+});
+clearButton.addEventListener('click',()=>
+{
+    field.innerHTML = '';
+    SetUpGrid(slider.value);
+});
 colours.forEach(colour => {
     colour.HTMLObject.addEventListener('click', ColourChoice);
 });
+
